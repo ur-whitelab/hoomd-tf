51199b7eb7ae004ca091cad69b979c9d5640e5ae
Andrew White <white.d.andrew@gmail.com>
2018-07-02 10:55:11 -0400

    POC for TF graph portion is complete

--- tensorflow_plugin/test-cases/tf-graph-surgery/IpcTester.h
+++ tensorflow_plugin/test-cases/tf-graph-surgery/IpcTester.h
@@ -1,12 +1,28 @@
 #include <hoomd/extern/pybind/include/pybind11/pybind11.h>
+#include <hoomd/extern/pybind/include/pybind11/stl_bind.h>
+#include <hoomd/extern/pybind/include/pybind11/stl.h>
+#include <hoomd/HOOMDMath.h>
+
 
 class IpcTester  {
 
     public:
-        IpcTester(pybind11::object& py_self);
+        IpcTester(pybind11::object& py_self, size_t length);
+        virtual ~IpcTester();
 
-        virtual void test();
+        std::vector<Scalar4> get_input_array() const;
+        std::vector<Scalar4> get_output_array() const;
+
+
+        int64_t get_input_buffer() const { return reinterpret_cast<int64_t> (_input_buffer);}
+        int64_t get_output_buffer() const {return reinterpret_cast<int64_t> (_output_buffer);}
+
         pybind11::object& _py_self;
+    private:
+        const size_t _length;
+        Scalar4* _input_buffer;
+        Scalar4* _output_buffer;
+
 };
 
 void export_IpcTester(pybind11::module& m);
--- tensorflow_plugin/test-cases/tf-graph-surgery/load_graph.py
+++ tensorflow_plugin/test-cases/tf-graph-surgery/load_graph.py
@@ -1,22 +1,17 @@
 import tensorflow as tf
 import numpy as np
+from ipc_tester import IpcTester
 
-#make a block of memory in C
-array = np.ones((32, 4), np.float32)
-array1 = [1.0 for _ in range(32 * 4)]
-address1 = id(array1)
-#array2 = np.zeros((32,4), np.float32)
-array2 = [0.0 for _ in range(32 * 4)]
-address2 = id(array2)
+ipct = IpcTester(32)
 ipc_to_tensor_module = tf.load_op_library('/srv/hoomd-blue/build/hoomd/tensorflow_plugin/ipc2tensor/lib_ipc2tensor_op.so')
 ipc_to_tensor = ipc_to_tensor_module.ipc_to_tensor
 tensor_to_ipc_module = tf.load_op_library('/srv/hoomd-blue/build/hoomd/tensorflow_plugin/tensor2ipc/lib_tensor2ipc_op.so')
 tensor_to_ipc = tensor_to_ipc_module.tensor_to_ipc
 
-graph_input = ipc_to_tensor(address=address1, size=32, T=np.float32)
-print(graph_input)
+graph_input = ipc_to_tensor(address=ipct.get_input_buffer(), size=32, T=np.float32)
 output_tensor = tf.Variable(expected_shape=(32, 4), name='output_tensor', dtype=np.float32, initial_value=0)
 
+print('prior', ipct.get_output_array())
 with tf.Session() as sess:
   new_saver = tf.train.import_meta_graph('model/test.meta', input_map={'input:0': graph_input})
   new_saver.restore(sess, tf.train.latest_checkpoint('./model'))
@@ -24,12 +19,10 @@ with tf.Session() as sess:
 
   print([n.name for n in tf.get_default_graph().as_graph_def().node])
   out = tf.get_default_graph().get_tensor_by_name('output:0')
-  #print(tf.get_default_graph().as_graph_def().SerializeToString())
-  #graph_output = tensor_to_ipc(out, address=address2, size=32)
+  graph_output = tensor_to_ipc(out, address=ipct.get_output_buffer(), size=32)
 
-  print(array2)
-  sess.run(tf.Print(out, [out, graph_input]))
-print(array2)
-
-with tf.Session() as sess:
-  sess.run(tf.Print(graph_input, [graph_input]))
+  #print(ipct.get_input_array())
+  print('about to print out')
+  p = tf.Print(out, [out, graph_input])
+  sess.run(graph_output)
+print('post', ipct.get_output_array())
