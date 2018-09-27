Fixed exploding gradients in safe_div. LJ_Pressure is broken (bisect needed)

tensorflow_plugin/graphbuilder.py
@@ -11,7 +11,7 @@ class graph_builder:
         self.atom_number = atom_number
         self.nneighbor_cutoff = nneighbor_cutoff
         #use zeros so that we don't need to feed to start session
-        self.nlist = tf.zeros ([atom_number, nneighbor_cutoff, 4], name='nlist')        
+        self.nlist = tf.zeros ([atom_number, nneighbor_cutoff, 4], name='nlist')
         self.virial = None
         self.positions = tf.zeros ([atom_number, 4], name='positions')
         if not output_forces:
@@ -35,14 +35,14 @@ class graph_builder:
             if nlist_forces is not None:
                 nlist_forces = tf.identity(2.0 * nlist_forces, name='nlist-pairwise-force-gradient-raw')
                 zeros = tf.zeros(tf.shape(nlist_forces))
-                nlist_forces = tf.where(tf.is_nan(nlist_forces), zeros, nlist_forces, name='nlist-pairwise-force-gradient')
+                nlist_forces = tf.where(tf.is_finite(nlist_forces), nlist_forces, zeros, name='nlist-pairwise-force-gradient')
                 nlist_reduce = tf.reduce_sum(nlist_forces, axis=1, name='nlist-force-gradient')
                 if virial:
                     with tf.name_scope('virial-calc'):
                         #now treat virial
                         nlist3 = self.nlist[:, :, :3]
                         rij_outter = tf.einsum('ijk,ijl->ijkl', nlist3, nlist3)
-                        #F / rs                        
+                        #F / rs
                         self.nlist_r_mag = graph_builder.safe_norm(nlist3 + 1.0e-15, axis=2, name='nlist-r-mag')
                         self.nlist_force_mag = graph_builder.safe_norm(nlist_forces + 1.0e-15, axis=2, name='nlist-force-mag')
                         F_rs = self.safe_div(self.nlist_force_mag, 2.0 * self.nlist_r_mag)
@@ -70,29 +70,18 @@ class graph_builder:
         return tf.identity(forces, name='computed-forces')
 
     @staticmethod
-    def safe_div(numerator, denominator, name='graphbuild-safe-div'):
-        '''Divides two values, returning 0 if the denominator is <= 0.
-        Args:
-            numerator: A real `Tensor`.
-            denominator: A real `Tensor`, with dtype matching `numerator`.
-            name: Name for the returned op.
-        Returns:
-            0 if `denominator` <= 0, else `numerator` / `denominator`
-        Taken from tensorflow/contrib/metrics/python/ops/metric_ops.py
+    def safe_div(numerator, denominator, delta=1e-6, **kwargs):
         '''
-        with tf.name_scope('graphbuilder-safe-div'):
-            op = tf.where(
-                tf.greater(denominator, 0),
-                tf.truediv(numerator, denominator),
-                tf.zeros_like(denominator),
-                name=name)
-
+        There are some numerical instabilities that occur during learning
+        when gradients are propagated. The delta is problem specific.
+        '''
+        op = tf.divide(numerator, denominator + delta, **kwargs)
         return op
 
     @staticmethod
-    def safe_norm(tensor, **kwargs):
+    def safe_norm(tensor, delta=1e-6, **kwargs):
         #https://github.com/tensorflow/tensorflow/issues/12071
-        return tf.norm(tensor + 1.0e-15, **kwargs)
+        return tf.norm(tensor + delta, **kwargs)
 
     def save(self, model_directory, force_tensor = None, virial = None, out_nodes=[]):
 

tensorflow_plugin/models/test-models/build.py
@@ -94,19 +94,22 @@ def trainable_graph(N, NN, name):
     graph = hoomd.tensorflow_plugin.graph_builder(N, NN)
     nlist = graph.nlist[:, :, :3]
     #get r
-    r = tf.norm(nlist, axis=2)
+    r = graph.safe_norm(nlist, axis=2)
     #compute 1 / r while safely treating r = 0.
-    #pairwise energy. Double count -> divide by 2    
-    epsilon = tf.Variable(4.0, name='lj-epsilon')
-    sigma = tf.Variable(4.0, name='lj-sigma')
+    #pairwise energy. Double count -> divide by 2
+    epsilon = tf.Variable(1.0, name='lj-epsilon')
+    sigma = tf.Variable(1.0, name='lj-sigma')
     tf.summary.scalar('lj-epsilon', epsilon)
     inv_r6 = graph.safe_div(sigma**6, r**6)
     p_energy = epsilon / 2.0 * (inv_r6 * inv_r6 - inv_r6)
     #sum over pairwise energy
     energy = tf.reduce_sum(p_energy, axis=1)
+    check = tf.check_numerics(p_energy, 'Your tensor is invalid')
     forces = graph.compute_forces(energy)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.0).minimize(energy)
-    graph.save(force_tensor=forces, model_directory=name, out_nodes=[optimizer])
+    tf.summary.histogram('forces', forces)
+    optimizer = tf.train.AdamOptimizer(1.0).minimize(energy)
+    #check = tf.add_check_numerics_ops()
+    graph.save(force_tensor=forces, model_directory=name, out_nodes=[optimizer, check])
 
 def bootstrap_graph(N, NN, directory):
     #make bootstrap graph
@@ -114,7 +117,7 @@ def bootstrap_graph(N, NN, directory):
     v = tf.Variable(8.0, name='epsilon')
     s = tf.Variable(2.0, name='sigma')
 
-    #save it    
+    #save it
     bootstrap_dir = os.path.join(directory, 'bootstrap')
     saver = tf.train.Saver()
     with tf.Session() as sess:

tensorflow_plugin/tfmanager.py
@@ -25,7 +25,7 @@ class TFManager:
     def __init__(self, graph_info, device,q, tasklock,
                 positions_buffer, nlist_buffer,
                 forces_buffer, virial_buffer, log_filename,
-                dtype, debug, write_tensorboard, use_feed, 
+                dtype, debug, write_tensorboard, use_feed,
                 bootstrap, bootstrap_map,
                 save_period):
 
@@ -152,7 +152,7 @@ class TFManager:
         self.summaries = tf.summary.merge_all()
         self.tb_writer = tf.summary.FileWriter(os.path.join(self.model_directory, 'tensorboard'),
                                       sess.graph)
-        tf.global_variables_initializer()
+        #tf.global_variables_initializer()
         self.out_nodes += [self.summaries]
 
 
@@ -164,21 +164,21 @@ class TFManager:
         config=tf.ConfigProto(gpu_options=gpu_options)
         with tf.Session(config=config) as sess:
             #resore model checkpoint if there are variables
-            if len(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)) > 0:                             
+            if len(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)) > 0:
                 #first initialize
                 self.log.info('Found trainable variables...')
                 sess.run(tf.global_variables_initializer())
                 self.log.info('Trainable vars initialized')
                 self.saver = tf.train.Saver()
-                if self.bootstrap is not None:                    
-                    checkpoint = tf.train.latest_checkpoint(self.bootstrap)                    
+                if self.bootstrap is not None:
+                    checkpoint = tf.train.latest_checkpoint(self.bootstrap)
                     if checkpoint is None:
                         raise ValueError('Could not find bootstrap checkpoint {}'.format(self.bootstrap))
                     self.log.info('Using bootstrap checkpoint {}'.format(self.bootstrap))
                     #convert bootstrap map values into actual variables
                     variable_map = None
                     if self.bootstrap_map is not None:
-                        variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)                        
+                        variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
                         variable_map = dict()
                         for k,vname in self.bootstrap_map.items():
                             value = None
@@ -188,7 +188,7 @@ class TFManager:
                                     value = v
                             if value is None:
                                 raise ValueError('Could not find variable {} in graph while processing bootstrap_map'.format(vname))
-                            variable_map[k] = value                  
+                            variable_map[k] = value
                     bootstrap_saver = tf.train.Saver(variable_map)
                     bootstrap_saver.restore(sess, checkpoint)
                 else: