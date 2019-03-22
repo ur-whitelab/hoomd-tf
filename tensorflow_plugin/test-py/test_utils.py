Fixed ordering/masking error in mapping nlist

tensorflow_plugin/test-py/test_utils.py
@@ -137,7 +137,40 @@ class test_mappings(unittest.TestCase):
         # TODO: Come up with a real test of this.
         assert True
 
+
     def test_compute_nlist(self):
+        N = 10
+        positions = tf.tile(tf.reshape(tf.range(N), [-1,1]), [1, 3])
+        print(positions.shape)
+        system = type('', (object, ), {'box': type('', (object,), {'Lx': 100., 'Ly': 100., 'Lz':100.})})
+        nlist = htf.compute_nlist(tf.cast(positions, tf.float32), 100., 9, system, True)
+        with tf.Session() as sess:
+            nlist = sess.run([nlist])
+            np.testing.assert_array_almost_equal(np.sort(nlist[0,:])[0], [9, 9, 9])
+            np.testing.assert_array_almost_equal(np.sort(nlist[-1,:])[-1], [-9, -9, -9])
+
+    def test_compute_nlist(self):
+        N = 10
+        positions = tf.tile(tf.reshape(tf.range(N), [-1,1]), [1, 3])
+        print(positions.shape)
+        system = type('', (object, ), {'box': type('', (object,), {'Lx': 100., 'Ly': 100., 'Lz':100.})})
+        nlist= htf.compute_nlist(tf.cast(positions, tf.float32), 100., 9, system, True)
+        with tf.Session() as sess:
+            nlist = sess.run(nlist)
+            np.testing.assert_array_almost_equal(np.sort(nlist[0,:])[0], [9, 9, 9])
+
+    def test_compute_nlist_cut(self):
+        N = 10
+        positions = tf.tile(tf.reshape(tf.range(N), [-1,1]), [1, 3])
+        print(positions.shape)
+        system = type('', (object, ), {'box': type('', (object,), {'Lx': 100., 'Ly': 100., 'Lz':100.})})
+        nlist = htf.compute_nlist(tf.cast(positions, tf.float32), 5.5, 9, system, True)
+        with tf.Session() as sess:
+            nlist = sess.run(nlist)
+            np.testing.assert_array_almost_equal(np.sort(nlist[0,:])[0], [3, 3, 3])
+            np.testing.assert_array_almost_equal(np.sort(nlist[-1,:])[0], [-3, -3, -3])
+
+    def test_nlist_compare(self):
         rcut = 5.0
         c = hoomd.context.initialize()
         # disable sorting
@@ -157,6 +190,12 @@ class test_mappings(unittest.TestCase):
 
             hoomd.run(100)
         variables  = hoomd.tensorflow_plugin.load_variables(model_dir, ['hoomd-r', 'htf-r'])
-        np.testing.assert_array_almost_equal(variables['hoomd-r'], variables['htf-r'])
+        # the two nlists need to be sorted to be compared
+        nlist = variables['hoomd-r']
+        cnlist = variables['htf-r']
+        for i in range(nlist.shape[0]):
+            ni = np.sort(nlist[i,:])
+            ci = np.sort(cnlist[i, :])
+            np.testing.assert_array_almost_equal(ni, ci, decimal=2)
 if __name__ == '__main__':
     unittest.main()

tensorflow_plugin/utils.py
@@ -237,7 +237,7 @@ def center_of_mass(positions, mapping, system, name='center-of-mass'):
     return tf.identity(thetamean  / np.pi / 2 * box_dim, name=name)
 
 
-def compute_nlist(positions, r_cut, NN, system):
+def compute_nlist(positions, r_cut, NN, system, sorted=False):
 
     M = tf.shape(positions)[0]
 
@@ -261,13 +261,13 @@ def compute_nlist(positions, r_cut, NN, system):
     mask = (dist <= r_cut) & (dist >= 5e-4)
     mask_cast = tf.cast(mask, dtype=dist.dtype)
     dist_mat_r = dist * mask_cast
-    topk = tf.math.top_k(dist_mat_r, k = NN, sorted = False)
+    topk = tf.math.top_k(dist_mat_r, k = NN, sorted = sorted)
 
     # we have the topk, but now we need to remove others
-    idx = tf.tile(tf.range(M),[NN])
-    idx = tf.reshape(idx,[-1,1])
+    idx = tf.tile(tf.reshape(tf.range(M), [-1,1]), [1, NN])
+    idx = tf.reshape(idx, [-1, 1])
     flat_idx = tf.concat([idx,tf.reshape(topk.indices,[-1,1])],-1)
-    nlist = tf.gather_nd(dist_mat,flat_idx)
+    nlist = tf.gather_nd(dist_mat * tf.reshape(mask_cast, [M, M,1]),flat_idx)
     nlist = tf.reshape(nlist,[-1,NN,3])
 
     return nlist