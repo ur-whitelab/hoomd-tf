fc9f2a07f9b377d9fddc44488d1bde2278f4b734
DILNOZA AMIRKULOVA <damirkul@bhd0038.bluehive.circ.private>
2019-04-23 14:59:47 -0400

    cleaned up the extra lines

--- tensorflow_plugin/models/example-models/eds/real_build_new.py
+++ tensorflow_plugin/models/example-models/eds/real_build_new.py
@@ -22,8 +22,7 @@ def make_eds_graph(N, NN, directory, cv_op, set_point):
     set_point=tf.Variable(set_point,name='set_point')#,dtype=tf.float64)
     colvar=tf.Variable(0.0, name='colvar_inst')          #col var for outputing the instantenous cv
     colvar_sq=tf.Variable(0.0, name='cv0')          #col var for outputing the instantenous cv^2
-    
-    aver_r,cv_sq,print_op_r,e,f=cv_op(graph,N)          #this outputs the mean cv, cv^2 and a print statement
+    aver_r,cv_sq=cv_op(graph,N)          #this outputs the mean cv, cv^2 and a print statement
     update_colvar_op = colvar.assign(aver_r)
     update_cv_op=colvar_sq.assign(cv_sq)
     run_cv=graph.running_mean(colvar,name='mean_cv')    #takes the running mean of the cv
@@ -43,32 +42,15 @@ def make_eds_graph(N, NN, directory, cv_op, set_point):
     forces = graph.compute_forces(bias_energy2) #computing the forces from the bias energy
     print_op=tf.print('bias energy is',bias_energy2,output_stream=sys.stdout)
     print_op_mean= tf.print('CV mean is', mean,output_stream=sys.stdout)
-    graph.save(model_directory=directory, out_nodes=[update_step_op,update_colvar_op,update_cv_op,print_op2,update_mean_op,print_op3,update_norm_gradient,update_alpha_op,print_op_mean,print_op_r,e,f],force_tensor=forces,virial=None)
+    graph.save(model_directory=directory, out_nodes=[update_step_op,update_colvar_op,update_cv_op,print_op2,update_mean_op,print_op3,update_norm_gradient,update_alpha_op,print_op_mean],force_tensor=forces,virial=None)
     
 def avg_r(graph,N):
     '''returns average distance from center of mass'''
     
-
-    # positions=tf.cast(positions1,tf.float64)
-    com=tf.reduce_mean(graph.positions[:,:2],0)#,name='com')#,dtype=tf.float64)
-    #com=tf.scalar_mul(1/N,total_position)
-    print_op=tf.print('center_of_mass is',com,output_stream=sys.stdout)
-    print_op2=tf.print('subtract',tf.math.subtract(graph.positions[:,:2],tf.reshape(tf.tile(com,[N]),(N,2))),output_stream=sys.stdout)
-    
-    aaaa=tf.print('tile',tf.tile(com,[N]),output_stream=sys.stdout)
-    aaa=tf.print('com in long term',tf.reshape(tf.tile(com,[N]),(N,2)),output_stream=sys.stdout)
-    rs= graph.safe_norm(tf.math.subtract(graph.positions[:,:2],com),axis=1)#,tf.reshape(tf.tile(com,[N]),(N,2))),axis=1)
-
-
-
-    
+    com=tf.reduce_mean(graph.positions[:,:2],0)
+    rs= graph.safe_norm(tf.math.subtract(graph.positions[:,:2],com),axis=1)
     real=tf.reduce_mean(rs)
-    a=tf.print('rs not reduce rs',rs,output_stream=sys.stdout)
-    b=tf.print('rs not reduce real ',real,output_stream=sys.stdout)
-    c=tf.print('rs not reduce com',com,output_stream=sys.stdout)
-    
-    return real,real**2,a,b,c
-
+    return real,real**2
     
 
 make_eds_graph(64, 30, 'manuscrpt/eds', avg_r,5.8)
