f03e3ffd6d24d3eb7eee0b49afadbe9a3aa92b2b
DILNOZA AMIRKULOVA <damirkul@bhd0038.bluehive.circ.private>
2019-04-23 14:53:13 -0400

    removed extra lines and corrected EDS

---
+++ tensorflow_plugin/models/example-models/eds/real_build_new.py
@@ -0,0 +1,75 @@
+import hoomd.tensorflow_plugin as htf
+import sys
+import tensorflow as tf
+
+
+out_aver=open('output_aver_r_running_mean.txt', 'a')
+def make_eds_graph(N, NN, directory, cv_op, set_point):
+    '''Currently only computes running mean'''
+
+    graph =htf.graph_builder(N-1,output_forces=True)
+
+    A=tf.constant(0.3,name='A',dtype=tf.float32)
+    kt=1.0
+    beta=1/kt
+    bias_energy=tf.Variable(tf.ones(N,dtype=tf.float32), dtype=tf.float32,name='energy')
+    steps = tf.Variable(1.0, name='steps')#,dtype=tf.float64)
+    mean = tf.Variable(0.1, name='mean')#,dtype=tf.float64)
+    #variance=tf.Variable(0.1, name='variance')#,dtype=tf.float64)
+    gradient=tf.Variable(0.1,name='gradient')#,dtype=tf.float64)
+    alpha=tf.Variable(0.001,name='alpha')#,dtype=tf.float64)
+    bias_val=tf.Variable(1.0,name='bias-val')#,expected_shape=(1),dtype=tf.float64)
+    set_point=tf.Variable(set_point,name='set_point')#,dtype=tf.float64)
+    colvar=tf.Variable(0.0, name='colvar_inst')          #col var for outputing the instantenous cv
+    colvar_sq=tf.Variable(0.0, name='cv0')          #col var for outputing the instantenous cv^2
+    
+    aver_r,cv_sq,print_op_r,e,f=cv_op(graph,N)          #this outputs the mean cv, cv^2 and a print statement
+    update_colvar_op = colvar.assign(aver_r)
+    update_cv_op=colvar_sq.assign(cv_sq)
+    run_cv=graph.running_mean(colvar,name='mean_cv')    #takes the running mean of the cv
+    variance=graph.running_mean(colvar_sq,name='cv2')-(run_cv**2)        #variance of the collective variable
+    gradient_new=-2*beta*(graph.safe_div(run_cv,set_point)-1)*(variance)  #g_tau (gradient at time tau) in the EDS paper aka eq 5
+    norm_gradient=(gradient_new**2+gradient**2)**0.5                 #sq root of the gradients squared aka A/learning_rate
+    learning_rate=graph.safe_div(A,norm_gradient)                    #learning rate
+    alpha_val=alpha-learning_rate*gradient_new                       #new coupling constant
+    print_op2=tf.print('alpha',alpha_val,output_stream=sys.stdout)   
+    print_op3=tf.print('aver_r after reduce mean',aver_r,output_stream=sys.stdout)
+    bias_potential=tf.reduce_sum((graph.safe_div((alpha_val*aver_r),set_point)))
+    update_step_op = steps.assign_add(1.0)
+    update_norm_gradient=gradient.assign(gradient_new)
+    update_alpha_op=alpha.assign(alpha_val)
+    update_mean_op=mean.assign(run_cv)
+    bias_energy2=bias_energy*bias_potential #expanding the bais potential to be for for n particles
+    forces = graph.compute_forces(bias_energy2) #computing the forces from the bias energy
+    print_op=tf.print('bias energy is',bias_energy2,output_stream=sys.stdout)
+    print_op_mean= tf.print('CV mean is', mean,output_stream=sys.stdout)
+    graph.save(model_directory=directory, out_nodes=[update_step_op,update_colvar_op,update_cv_op,print_op2,update_mean_op,print_op3,update_norm_gradient,update_alpha_op,print_op_mean,print_op_r,e,f],force_tensor=forces,virial=None)
+    
+def avg_r(graph,N):
+    '''returns average distance from center of mass'''
+    
+
+    # positions=tf.cast(positions1,tf.float64)
+    com=tf.reduce_mean(graph.positions[:,:2],0)#,name='com')#,dtype=tf.float64)
+    #com=tf.scalar_mul(1/N,total_position)
+    print_op=tf.print('center_of_mass is',com,output_stream=sys.stdout)
+    print_op2=tf.print('subtract',tf.math.subtract(graph.positions[:,:2],tf.reshape(tf.tile(com,[N]),(N,2))),output_stream=sys.stdout)
+    
+    aaaa=tf.print('tile',tf.tile(com,[N]),output_stream=sys.stdout)
+    aaa=tf.print('com in long term',tf.reshape(tf.tile(com,[N]),(N,2)),output_stream=sys.stdout)
+    rs= graph.safe_norm(tf.math.subtract(graph.positions[:,:2],com),axis=1)#,tf.reshape(tf.tile(com,[N]),(N,2))),axis=1)
+
+
+
+    
+    real=tf.reduce_mean(rs)
+    a=tf.print('rs not reduce rs',rs,output_stream=sys.stdout)
+    b=tf.print('rs not reduce real ',real,output_stream=sys.stdout)
+    c=tf.print('rs not reduce com',com,output_stream=sys.stdout)
+    
+    return real,real**2,a,b,c
+
+    
+
+make_eds_graph(64, 30, 'manuscrpt/eds', avg_r,5.8)
+out_aver.close()
---
+++ tensorflow_plugin/models/example-models/eds/real_run_new.py
@@ -0,0 +1,29 @@
+import hoomd, hoomd.md, hoomd.tensorflow_plugin
+import tensorflow as tf
+
+
+with hoomd.tensorflow_plugin.tfcompute('manuscrpt/eds') as tfcompute:
+    hoomd.context.initialize()
+
+    N = 64
+    NN = 30
+    rcut = 3.0
+    system = hoomd.init.create_lattice(unitcell=hoomd.lattice.sq(a=2.0),
+                                       n=[8,8])
+    hoomd.md.update.enforce2d()
+    nlist = hoomd.md.nlist.cell(check_period = 1)
+
+    lj = hoomd.md.pair.lj(rcut, nlist)
+    lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
+    hoomd.md.integrate.mode_standard(dt=0.005)
+    hoomd.md.integrate.nvt(kT=1.0,tau=0.5,
+        group=hoomd.group.all())
+    hoomd.analyze.log(filename='eds.log',
+                      quantities = ['momentum', 'temperature', 'time'],
+                      period=100,overwrite=True)
+
+    tfcompute.attach(nlist, r_cut=rcut)
+    #hoomd.dump.gsd(filename="trajectory_eds.gsd", period=10)
+    hoomd.dump.gsd(filename="saveall_running.gsd", overwrite=True, period=100, group=hoomd.group.all(), dynamic=['attribute', 'momentum', 'topology'])
+    
+    hoomd.run(200000)
