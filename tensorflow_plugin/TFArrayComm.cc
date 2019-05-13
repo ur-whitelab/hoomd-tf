2a7b391f26414ed12d25b288279ed24a3a0bb845
Andrew White <white.d.andrew@gmail.com>
2019-05-13 14:59:18 -0400

    Made TFArrayComm use exec_conf so that cuda error checking can be done

--- tensorflow_plugin/TFArrayComm.cc
+++ tensorflow_plugin/TFArrayComm.cc
@@ -8,24 +8,8 @@
 
 namespace hoomd_tf{
 
-#ifdef ENABLE_CUDA
 
-void tf_check_cuda_error(cudaError_t err, const char* file,
-                          unsigned int line) {
-  // if there was an error
-  if (err != cudaSuccess) {
-    // print an error message
-    std::cerr << "error type: " << cudaGetErrorName(err) << ": "
-              << std::string(cudaGetErrorString(err)) << " before " << file
-              << ":" << line << std::endl;
-
-    // throw an error exception
-    throw(std::runtime_error("CUDA Error"));
-  }
-}
-#endif
-
-  void* int2ptr(int64_t address) { return reinterpret_cast<void*>(address); }
+void* int2ptr(int64_t address) { return reinterpret_cast<void*>(address); }
 
 void export_TFArrayComm(pybind11::module& m) {
   pybind11::class_<TFArrayComm<TFCommMode::CPU, double>,
