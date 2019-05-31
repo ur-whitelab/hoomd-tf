928b629d8d46c02480910a2e35fd12e55c03edbd
RainierBarrett <rainier.barrett@gmail.com>
2019-05-30 22:20:38 -0400

    Added a failing C++ test. Compile with -DBUILD_TESTING=ON

--- htf/test-cc/test_commstruct.cc
+++ htf/test-cc/test_commstruct.cc
@@ -1,4 +1,5 @@
-#include "hoomd/tensorflow_plugin/CommStruct.h"
+#include "hoomd/ExecutionConfiguration.h"
+#include "hoomd/htf/CommStruct.h"
 
 /*! \file test_commstruct.cc
     \brief Implements unit tests for the CommStruct object class
@@ -7,15 +8,19 @@
 
 using namespace std;
 
-#include "hoomd/test/up11_config.h"
+#include "hoomd/test/upp11_config.h"
 HOOMD_UP_MAIN();
 
 UP_TEST( CommStruct_basic_tests)
     {
-    int dims = 1;
+    unsigned int n_elems = 1;
+    const ExecutionConfiguration *exec_conf = new ExecutionConfiguration();
+    std::shared_ptr<const ExecutionConfiguration> conf_ptr;
+    const std::string tag = "test";
+    GlobalArray<Scalar> test_array(n_elems, conf_ptr, tag);
     size_t elem_size = sizeof(Scalar);
     const char* name = "test_commstruct";
-    CommStruct test_struct(dims, elem_size, name);
+    hoomd_tf::CommStructDerived<Scalar> test_struct(test_array, name);
     UP_ASSERT(test_struct.name == name);
     UP_ASSERT(test_struct.element_size == elem_size);
     // mem_size should default to 0 because it's empty
