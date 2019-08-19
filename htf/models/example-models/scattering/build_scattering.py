6c7f9a28c37b59f873031cdea25fab441220f619
Heta Gandhi <hgandhi@ur.rochester.edu>
2019-08-19 11:47:54 -0400

    Updated the scattering code for water (#82)
    * Updated the scattering code to be consistent with description in paper

--- htf/models/example-models/scattering/build_scattering.py
+++ htf/models/example-models/scattering/build_scattering.py
@@ -1,27 +1,58 @@
 import tensorflow as tf
 from hoomd.htf import graph_builder
+import pickle
+import gsd
+import gsd.hoomd
+import numpy as np
 import sys
 if(len(sys.argv) != 2):
     print('Usage: build_scattering.py [model_dir]')
     exit(0)
 
 model_dir = sys.argv[1]
-b = -3.739  # assuming bunch of Hydrogens
-N = 64
-graph = graph_builder(N, N-1, output_forces=False)
+
+struct_dir = '/scratch/hgandhi/run_scattering/'
+
+# load topology information
+with open(struct_dir + 'water.pickle', 'rb') as f:
+    param_sys, _ = pickle.load(f)
+g = gsd.hoomd.open(struct_dir + 'water.gsd')
+frame = g[0]
+
+NN = 256
+# build scattering cross-section
+data = [None for _ in range(NN)]
+# Neutron Scattering lengths (NIST)
+# Hydrogen
+data[1] = -3.742
+# Carbon
+data[6] = 6.646
+# Oxygen
+data[8] = 5.805
+N = len(param_sys.atoms)
+cross = np.zeros(len(frame.particles.types), dtype=np.float32)
+
+for a in param_sys.atoms:
+    cross[frame.particles.types.index(a.type)] = data[a.element]
+
+graph = graph_builder(NN, output_forces=False)
+# get pairwise scattering length bi*bj
+bj = tf.gather(cross, tf.cast(graph.nlist[:, :, 3], tf.int32))
+bi = tf.gather(cross, tf.cast(graph.positions[:, 3], tf.int32))
+bij = tf.einsum('ij,i -> ij', bj, bi)
 # get neighbor list
 nlist = graph.nlist[:, :, :3]
 # get interatomic distances
 r = tf.norm(nlist, axis=2)
-q = tf.lin_space(0., 10., 100)
+q = tf.lin_space(0.01, 10., 100)
 # q_rij must be QxNxNN - Outer product
 # qr[i, j, k] = q[i] * r[j,k]
 qr = tf.einsum('i, jk -> ijk', q, r)
 intensities = tf.multiply(0.5, tf.reduce_sum(
-        tf.reduce_sum(b*b*tf.sin(qr)*graph_builder.safe_div(
+        tf.reduce_sum(bij*tf.sin(qr)*graph_builder.safe_div(
                 1., qr), axis=2), axis=1))
 # print_node = tf.Print(intensities, [ intensities], summarize=1000)
-
+# Keep a running average of intensity
 avg = tf.Variable(tf.zeros(tf.shape(q)), name='intensity')
 steps = tf.Variable(1., name='n')
 steps_1 = tf.assign_add(steps, 1.)
--- htf/models/example-models/scattering/run_scattering.py
+++ htf/models/example-models/scattering/run_scattering.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 import hoomd
+import gsd.hoomd
+import pickle
+import hoomd.md
+import hoomd_ff
 from hoomd.htf import tfcompute
 import sys
 if(len(sys.argv) != 2):
@@ -7,28 +11,58 @@ if(len(sys.argv) != 2):
     exit(0)
 
 model_dir = sys.argv[1]
+struct_dir = '/scratch/hgandhi/run_scattering/'
+
+with gsd.hoomd.open(struct_dir + 'water.gsd') as g:
+    frame = g[0]
+    boxd = frame.configuration.box[0]
+
+with open(struct_dir + 'water.pickle', 'rb') as f:
+    param_sys, kwargs = pickle.load(f)
 
 
 with hoomd.htf.tfcompute(model_dir, _mock_mode=False,
-                                       write_tensorboard=False) as tfcompute:
+                         write_tensorboard=False) as tfcompute:
     hoomd.context.initialize("")
-    N = 8*8
-    NN = N - 1
-    r_cut = 1
-    hoomd.init.create_lattice(unitcell=hoomd.lattice.sc(a=1.0), n=4)
-    nlist = hoomd.md.nlist.cell(check_period=5)
-    lj = hoomd.md.pair.lj(r_cut=r_cut, nlist=nlist)
-    lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
-    hoomd.md.integrate.mode_standard(dt=0.005)
+    r_cut = 4.0
+    N = 5000
+    NN = 256
+    system = hoomd.init.read_gsd(struct_dir + 'water.gsd')
+    nlist = hoomd.md.nlist.cell(check_period=1)
+    hoomd_ff.pair_coeffs(frame, param_sys, nlist, r_cut=4.0)
+    # set-up pppm
+    charged = hoomd.group.all()
+    pppm = hoomd.md.charge.pppm(nlist=nlist, group=charged)
+    pppm.set_params(Nx=32, Ny=32, Nz=32, order=6, rcut=4.0)
+    # set-up bonds
+    hoomd_ff.bond_coeffs(frame, system, param_sys)
+    # set-up angles
+    hoomd_ff.angle_coeffs(frame, param_sys)
+    # set-up dihedrals
+    hoomd_ff.dihedral_coeffs(frame, param_sys)
+    # free particles from rigid bodies since rigid doesn't quite work for us
+    for i in range(frame.particles.N):
+        system.particles[i].body = -1
     all = hoomd.group.all()
-    hoomd.md.integrate.langevin(group=all, kT=0.2, seed=42)
-    tfcompute.attach(nlist, r_cut=r_cut, force_mode='ignore')
-    hoomd.analyze.log(filename='scattering.log',
-                      quantities=['momentum', 'temperature'],
-                      period=10,
-                      overwrite=True)
-    hoomd.dump.gsd("trajectory.gsd", period=2e3, group=all, overwrite=True)
-    hoomd.run(100)
+    kT = 1.9872 / 1000
+    fire = hoomd.md.integrate.mode_minimize_fire(dt=0.5 / 48.9,
+                                                 ftol=1e-4, Etol=1e-8)
+    nve = hoomd.md.integrate.nve(group=all)
+    for i in range(5):
+        if not(fire.has_converged()):
+            hoomd.run(100)
+    nve.disable()
+    # Now NVT
+    hoomd.md.integrate.mode_standard(dt=2 / 48.9)
+    nvt = hoomd.md.integrate.nvt(group=all, kT=298 * kT, tau=100 / 48.9)
+    nvt.randomize_velocities(1234)
+    tfcompute.attach(nlist, r_cut=r_cut, period=1)
+    # hoomd.analyze.log(filename='scattering.log',
+    #                  quantities=['momentum', 'temperature'],
+    #                  period=10,
+    #                  overwrite=True)
+    hoomd.dump.gsd("trajectory.gsd", period=10, group=all, overwrite=True)
+    hoomd.run(1e4)
     positions_array = tfcompute.get_positions_array()
     tfcompute.get_nlist_array()
     # print('positions array is {}'.format(positions_array))
