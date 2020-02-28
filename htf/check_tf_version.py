2221dbdadba1b616e5d68469966e5b9349df27e9
Andrew White <white.d.andrew@gmail.com>
2020-02-28 14:36:36 -0500

    pep8 strikes again

--- htf/check_tf_version.py
+++ htf/check_tf_version.py
@@ -16,11 +16,13 @@ if len(sys.argv) == 3 and len(sys.argv[2]) > 1:
     vcomp_req = parse_version(sys.argv[2])
 
     if vcomp.base_version.split('.')[0] != vcomp_req.base_version.split('.')[0]:
-        print('-- Error: tensorflow was compiled with compiler version {} but you are trying to build with {} (major mismatch). To force compilation add IGNORE_HTF_COMPILER flag'.format(vcomp, vcomp_req))
+        print('-- Error: tensorflow was compiled with compiler version {} '
+              'but you are trying to build with {} (major mismatch). To force'
+              'compilation add IGNORE_HTF_COMPILER flag'.format(vcomp, vcomp_req))
         exit(1)
     elif vcomp != vcomp_req:
-        print('-- Warning: tensorflow was compiled with compiler version {} but you are trying to build with {} (minor mismatch)'.format(vcomp, vcomp_req))
-    
+        print('-- Warning: tensorflow was compiled with compiler version {} '
+              'but you are trying to build with {} (minor mismatch)'.format(vcomp, vcomp_req))
 
 # get and parse the version of the detected TF version
 vtf = parse_version(tensorflow.__version__)
