f63a559c4a181c7e26f19717023ee14049b09d60
Andrew White <white.d.andrew@gmail.com>
2020-03-08 13:03:00 -0400

    pep8 style

--- htf/test-py/test_mpi_tensorflow.py
+++ htf/test-py/test_mpi_tensorflow.py
@@ -8,6 +8,7 @@ import build_examples
 import numpy as np
 import tempfile
 
+
 def run_tf_lj(N, T, directory = '/tmp/test-lj-potential-model'):
     model_dir = build_examples.lj_graph(N - 1, directory)
     with hoomd.htf.tfcompute(model_dir,
