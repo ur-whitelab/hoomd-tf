f9cf8f08703a9f89a11ae4017461de5f26aa5357
Rainier Barrett <rbarret8@ur.rochester.edu>
2020-03-18 17:49:44 -0400

    Update config file to support new and old sphinx versions

--- sphinx-docs/source/conf.py
+++ sphinx-docs/source/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 import mock
 from copy import copy
+import sphinx
+sphinx_ver = tuple(map(int, sphinx.__version__.split('.')))
 
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../htf'))
@@ -44,7 +46,13 @@ Maghesree Chakraborty, Heta Gandhi, Andrew D. White'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax'
 ]
+
+if sphinx_ver < (1,8,0):
+    autodoc_default_flags = ['inherited-members']
+else:
+    autodoc_default_options = {'inherited-members': None}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
