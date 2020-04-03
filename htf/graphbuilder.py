df5106b9c31e5022537dfbe996043c52f2d35a53
Mehrad Ansari <mehrad.ans@gmail.com>
2020-04-02 21:58:59 -0400

    made compatible with hoomd format

--- htf/graphbuilder.py
+++ htf/graphbuilder.py
@@ -186,13 +186,13 @@ class graph_builder:
             :return: The wrapped vector as a TF tensor
         """
         tilted_axis = ['x axis', 'y axis', 'z axis']
-
-
-    for i in range(3):
-        tilt_value = tf.gather_nd(self.box, [2, i])
-        if tilt_value != 0:
-            raise Exception(
-                'Simulation box is tilted in {}. Current version of Hoomd-tf cannot work with skewed boxes.'.format(tilted_axis[i]))
+        tilt_value = [self.box.xy, self.box.xz, self.box.yz]
+        print (tilt_value)
+        for i in range(3):
+            #tilt_value = tf.gather_nd(self.box.[i], [2, i])
+            if tilt_value[i] != 0:
+                raise Exception(
+                    'Simulation box is tilted in {}. Current version of Hoomd-tf cannot work with skewed boxes.'.format(tilted_axis[i]))
 
 
 
