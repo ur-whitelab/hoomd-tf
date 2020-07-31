d1804628a1c6b9d2e4918766f6808830ae45d8c2
Andrew White <white.d.andrew@gmail.com>
2020-07-31 04:00:03 -0400

    Fixed sanity unit test segfault

--- htf/test-py/test_sanity.py
+++ htf/test-py/test_sanity.py
@@ -25,11 +25,11 @@ class test_op(unittest.TestCase):
         import hoomd
         import hoomd.htf
         import tensorflow as tf
-        import numpy as np
         from hoomd.htf.tfmanager import load_htf_op_library
         hoomd_to_tf_module = load_htf_op_library('hoomd2tf_op')
         hoomd_to_tf = hoomd_to_tf_module.hoomd_to_tf
-
+        # don't want a segfault
+        tf.compat.v1.disable_eager_execution()
         with tf.device("/cpu:0"):
             tf_op = hoomd_to_tf(address=0,
                                 shape=[2], T=tf.float64,
