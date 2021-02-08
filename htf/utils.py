3975a2bf480a4b307c35c61bbf577061238de050
geemi725 <gwellawatte@gmail.com>
2021-02-07 23:33:58 -0500

    pep8

--- htf/utils.py
+++ htf/utils.py
@@ -206,9 +206,9 @@ def find_molecules(system):
 
 
 def find_molecules_from_topology(
-    universe,
-    atoms_in_molecule_list,
-     selection='all'):
+        universe,
+        atoms_in_molecule_list,
+        selection='all'):
     R""" Given a universe from MDAnaylis and list of atoms in every molecule type
      in the system,return a mapping from molecule index to particle index.
     Depending on the size of your system, this fuction might be slow to run.
@@ -480,46 +480,46 @@ def iter_from_trajectory(
                 lambda ag: [ag.positions.copy()], p).run().results
             # Construct new_trajectory from the MemoryReader explicitly:
             new_traj = MDAnalysis.coordinates.memory.MemoryReader(
-                x[:, 0], dimensions = dimensions, dt = dt)
+                x[:, 0], dimensions=dimensions, dt=dt)
         else:
             # Include positions, velocities and forces:
-            xvf=AnalysisFromFunction(lambda ag: [ag.positions.copy(
+            xvf = AnalysisFromFunction(lambda ag: [ag.positions.copy(
             ), ag.velocities.copy(), ag.forces.copy()], p).run().results
             new_traj = MDAnalysis.coordinates.memory.MemoryReader(
-                xvf[:, 0], velocities = xvf[:, 1], forces = xvf[:, 2], dimensions = dimensions, dt = dt)
-        universe.trajectory=new_traj
+                xvf[:, 0], velocities=xvf[:, 1], forces=xvf[:, 2], dimensions=dimensions, dt=dt)
+        universe.trajectory = new_traj
         print('The universe was redefined based on the atom group selection input.')
     # read trajectory
     # Modifying the universe for non 'all' atom selections.
-    box=universe.dimensions
+    box = universe.dimensions
     # convert lattice angles to tilt factors!
-    a=box[0]
-    b=1  # box[1]
-    c=1  # box[2]
-    alpha=np.deg2rad(box[3])
-    beta=np.deg2rad(box[4])
-    gamma=np.deg2rad(box[5])
-    lx=a
-    xy=1. / np.tan(gamma)
-    xz=c * np.cos(beta)
-    yz=(b * c * np.cos(alpha) - xy * xz)
+    a = box[0]
+    b = 1  # box[1]
+    c = 1  # box[2]
+    alpha = np.deg2rad(box[3])
+    beta = np.deg2rad(box[4])
+    gamma = np.deg2rad(box[5])
+    lx = a
+    xy = 1. / np.tan(gamma)
+    xz = c * np.cos(beta)
+    yz = (b * c * np.cos(alpha) - xy * xz)
     # define the system
-    hoomd_box=np.array([[0, 0, 0], [box[0], box[1], box[2]], [xy, xz, yz]])
+    hoomd_box = np.array([[0, 0, 0], [box[0], box[1], box[2]], [xy, xz, yz]])
     # make type array
     # Select atom group to use in the system
-    atom_group=universe.select_atoms(selection)
+    atom_group = universe.select_atoms(selection)
     # get unique atom types in the selected atom group
     try:
-        types=list(np.unique(atom_group.atoms.types))
+        types = list(np.unique(atom_group.atoms.types))
         # associate atoms types with individual atoms
-        type_array=np.array([types.index(i)
+        type_array = np.array([types.index(i)
                                for i in atom_group.atoms.types]).reshape(-1, 1)
     except MDAnalysis.exceptions.NoDataError:
-        type_array=np.zeros(len(atom_group)).reshape(-1, 1)
+        type_array = np.zeros(len(atom_group)).reshape(-1, 1)
 
     # define nlist operation
     # box_size = [box[0], box[1], box[2]]
-    nlist=compute_nlist(
+    nlist = compute_nlist(
         atom_group.positions,
         r_cut=r_cut,
         NN=nneighbor_cutoff,
@@ -531,6 +531,7 @@ def iter_from_trajectory(
                 (atom_group.positions,
                  type_array),
                 axis=1), hoomd_box, 1.0], ts
+
 
 def matrix_mapping(molecule, beads_distribution, mass_weighted=True):
     R''' This will create a M x N mass weighted mapping matrix where M is the number
@@ -553,15 +554,16 @@ def matrix_mapping(molecule, beads_distribution, mass_weighted=True):
     index = 0
     for s in range(M):
         for i, atom in enumerate(mapping_operator[s]):
-            CG_matrix[s, i+index] = [v for k,
-                                     v in Mws_dict.items() if atom in k][0]
+            CG_matrix[s, i + index] = [v for k,
+                                       v in Mws_dict.items() if atom in k][0]
         index += np.count_nonzero(CG_matrix[s])
-        CG_matrix[s] = CG_matrix[s]/np.sum(CG_matrix[s])
-    # Cheking that all atoms in the topology are included in the beads mapping list:
+        CG_matrix[s] = CG_matrix[s] / np.sum(CG_matrix[s])
+    # Cheking that all atoms in the topology are included in the beads mapping
+    # list:
     assert index == molecule.n_atoms, 'Number of atoms in the beads mapping list does \
      not match the number of atoms in topology.'
     if mass_weighted is True:
-       return CG_matrix
+        return CG_matrix
     else:
         no_mass_mapping = np.where(CG_matrix == 0, CG_matrix, 1)
         return CG_matrix, no_mass_mapping
