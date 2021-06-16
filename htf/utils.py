3358a2cff3aa2d17abec3604d4f0bf2c2c307e14
RainierBarrett <rainierbarrett@boisestate.edu>
2021-06-16 16:25:31 -0400

    Dropped a space in utils

--- htf/utils.py
+++ htf/utils.py
@@ -870,7 +870,7 @@ def mol_angle(
             cos_a = np.divide(
                 cos_a, (np.linalg.norm(wrap_vij) * np.linalg.norm(wrap_vjk)))
             cg_angles = np.arccos(cos_a)
-       return cg_angles
+        return cg_angles
 
 
 def mol_bond_distance(
